48649e1b4926857cb3a50af86f0b84adba7384b6
Kinza Syed Hussain <kinzasyed060@gmail.com>
2026-08-27 23:40:51 +0300

    Basic game

---
+++ script.js
@@ -0,0 +1,11 @@
+function showLevelSelect() {
+  document.getElementById('start-screen').classList.remove('active');
+  document.getElementById('level-screen').classList.add('active');
+}
+
+function startLevel1() {
+  document.getElementById('level-screen').classList.remove('active');
+  document.getElementById('game-screen').classList.add('active');
+}
+
+
