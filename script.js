ae72ad386eaa594c8bd222831ecf6e8e96244448
Kinza Syed Hussain <kinzasyed060@gmail.com>
2026-08-28 18:14:21 +0300

    some changes

--- script.js
+++ script.js
@@ -10,13 +10,52 @@ function startLevel1() {
 
 
 function parentsLeave() {
-  document.querySelector('.banana-dad').styledisplay = 'none';
-  document.querySelector('.banana-mom').styledisplay = 'none';
-  document.querySelector('.speech-bubble').innerHTML = "<strong>Banana Kid:</strong> Now I'm home alone! Time to check the house...";
+  const hallbg = document.querySelector('.bg-img');
+  const dad = document.querySelector('.banana-dad');
+  const mom = document.querySelector('.banana-mom');
+  const introbox = document.getElementById('intro-box');
+  const btn = document.getElementById('dialogue-btn');
 
-  const btn = document.querySelector('#intro-ui-box .game-btn');
-  btn.innerText = "Look Around";
-  btn.onclick = function() {
-    alert("Next scene ready!");
-  };
+  hallbg.src = "open door.jpeg";
+  if (dad) dad.style.display = 'none';
+  if (mom) mom.style.display = 'none';
+  if (introbox) introbox.style.display = 'none';
+
+  setTimeout(() => {
+    hallbg.src = "withoutopen door.jpeg";
+
+    if (introbox) introbox.style.display = 'block';
+    const speechBubble = document.querySelector('.speech-bubble');
+    if (speechBubble) {
+      speech-bubble.innerHTML = "<strong>Banana Kid:</strong> Bye Mom and Dad! Now I'm home alone! Time to check the house...";
+    }
+
+    if (btn) {
+      btn.innerText = "Wait... Who is at the door?";
+      btn.onclick = strangerArrives;
+    }
+  }, 1500);
 }
+
+function strangerArrives() {
+  const introbox = document.getElementById('intro-box');
+
+  if (introbox) {
+    introbox.innerHTML = `
+      <p id="dialogue-text"><strong>Stranger:</strong> *Knock Knock* "Hey kid! I'm Appy, open the door!"</p>
+      <div id="choices-container" style="display:flex; justify-content:center; gap:15px; margin-top:10px;">
+        <button class="game-btn danger" onclick="chooseDoor(true)">YES (Open Door)</button>
+        <button class="game-btn safe" onclick="chooseDoor(false)">NO (Keep Locked)</button>
+      </div>
+    `;
+  }
+
+  if (!document.getElementById('kitchen-arrow')) {
+    const arrow = document.createElement('button');
+    arrow.id = 'kitchen-arrow';
+    arrow.innerText = '<--- Go to kitchen (check clue)';
+    arrow.onclick = openKitchen;
+    document.getElementById('game-screen').appendChild(arrow);
+  }
+}
+  
